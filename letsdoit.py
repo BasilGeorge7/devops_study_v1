57d7bd9ec03effbd9a6caf172147eb2a6f578243
Basil George <87349509+BasilGeorge7@users.noreply.github.com>
2021-10-13 23:53:39 +0530

    Update letsdoit.py

--- letsdoit.py
+++ letsdoit.py
@@ -1,12 +1,1 @@
-import pandas as pd
-
-
-df=read_csv("username.csv", sep=";")
-df["age"]=''
-for x in df.index:
-   
-    
-    def function:
-        if 
-    
-    k=x
+print("hello good morning")
